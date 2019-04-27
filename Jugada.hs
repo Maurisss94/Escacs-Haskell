d6480207a9ecafa2d7ddea06a9888df77d182412
Maurisss94 <maurobale94@hotmail.es>
2019-04-27 16:12:12 +0200

    Modificant data Jugada

--- Jugada.hs
+++ Jugada.hs
@@ -7,7 +7,12 @@ import Peca
 import Posicio
 
 data Accio = Escac | EscacIMat | Res deriving (Eq, Show)
-data Jugada = Jugada Peca Posicio Posicio Accio | Acabada deriving (Eq, Show)
+data Jugada = Jugada {
+    tipusPeca :: Peca,
+    origen :: Posicio,
+    desti :: Posicio,
+    accio :: Accio
+    } | Acabada deriving (Eq, Show)
 
 
     
